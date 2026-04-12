caff6cb7c90478b5852e4a81dbcbb458eb9a52d6
AryanShrestha123 <aryan4845i@gmail.com>
2026-04-12 19:18:52 +0545

    pom.xml file updated and API setup with server and base path configuration

--- src/main/java/com/smartcampus/SmartCampusApi.java
+++ src/main/java/com/smartcampus/SmartCampusApi.java
@@ -4,13 +4,44 @@
 
 package com.smartcampus;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Logger;
+
 /**
  *
  * @author LEGION
  */
 public class SmartCampusApi {
+    
+    private static final Logger LOGGER = Logger.getLogger(SmartCampusApi.class.getName());
+    public static final String BASE_URI = "http://0.0.0.0:8080/";
+    
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig()
+                .packages(
+                        "com.smartcampus.resource",
+                        "com.smartcampus.exception",
+                        "com.smartcampus.filter"
+                );
+        
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        LOGGER.info("----------------------------------------");
+        LOGGER.info("Smart Campus API started");
+        LOGGER.info("URL: "+ BASE_URI);
+        LOGGER.info("Press enter to stop the server..");
+        LOGGER.info("----------------------------------------");
+        System.in.read();
+        server.stop();
+        LOGGER.info("Server stopped.");
+        
     }
 }
