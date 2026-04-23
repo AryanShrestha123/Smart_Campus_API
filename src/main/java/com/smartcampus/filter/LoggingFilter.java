1da16884aec013c9a99b501ab1aeee331c86f70e
AryanShrestha123 <aryan4845i@gmail.com>
2026-04-23 17:31:34 +0545

    Implemented JAX-RS logging filter for request and response tracking

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.filter;
+
+//Imports
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author LEGION
+ */
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter{
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException{
+        LOGGER.info(String.format("[REQUEST] Method=%s URI=%s",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri()));
+    }
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException{
+        LOGGER.info(String.format("[REQUEST] Method=%s URI=%s Status=HTTP %d",
+                requestContext.getMethod(),
+                requestContext.getUriInfo().getRequestUri(),
+                responseContext.getStatus()));
+    }
+}
